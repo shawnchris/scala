7206e6246a5c2c66b11aa163dcdc7dc9328c4af9
Shawn <shawn.chris.gao@outlook.com>
2023-02-16 18:45:46 -0800

    A0049GroupAnagrams

---
+++ leetcode/src/main/scala/A0049GroupAnagrams.scala
@@ -0,0 +1,15 @@
+class A0049GroupAnagrams extends App {
+  def groupAnagrams(strs: Array[String]): List[List[String]] = {
+    if (strs.length == 0) {
+      return List()
+    }
+
+    val map = scala.collection.mutable.HashMap.empty[String, List[String]]
+    for (str <- strs) {
+      val key = str.toCharArray.sorted.mkString
+      val value = map.getOrElse(key, List()) :+ str
+      map.put(key, value)
+    }
+    map.values.toList
+  }
+}
